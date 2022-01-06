4831b0b5428054ea72d2b69cba5f337dfe5ab5cf
16121a1228 <gjyothsna1228@gmail.com>
2022-01-06 10:08:55 +0530

    commit

---
+++ pro/src/App.js
@@ -0,0 +1,40 @@
+//import logo from './logo.svg';
+import Home from './ components/home';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+       <h2>pronutrition</h2>
+     </header>
+     <section>
+       <Home/>
+     </section>
+    </div>
+  );
+}
+
+export default App;
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
